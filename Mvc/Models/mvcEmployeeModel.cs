280e0f0906cfe51bc169e3d9f361eb9be195044c
Nikita <nikitajaiswal23@gmail.com>
2022-06-03 14:40:40 +0530

    Add project files.

---
+++ Mvc/Models/mvcEmployeeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc.Models
+{
+    public class mvcEmployeeModel
+    {
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public string Position { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public Nullable<int> Age { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public Nullable<int> Salary { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+        public Nullable<int> CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+        public Nullable<int> ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifedOn { get; set; }
+    }
+}
